634107c5e82543ac6a6361f1bcca4bdacdbee9ce
Peter Levinsky <pele@easj.dk>
2017-12-19 10:55:57 +0100

    Add project files.

---
+++ ClassDemoCatches/IService1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using ModelLibrary;
+
+namespace ClassDemoCatches
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        [WebInvoke(
+                Method = "GET",
+                ResponseFormat = WebMessageFormat.Json,
+                UriTemplate = "catches")
+        ]
+        List<Catch> GetCatches();
+
+        [WebInvoke(
+                Method = "GET",
+                ResponseFormat = WebMessageFormat.Json,
+                UriTemplate = "catch/{id}")
+        ]
+        Catch GetOneCatch(String id);
+
+        [WebInvoke(
+                Method = "POST",
+                RequestFormat = WebMessageFormat.Json,
+                //ResponseFormat = WebMessageFormat.Json,
+                UriTemplate = "catches")
+        ]
+        void AddCatch(Catch newCatch);
+
+        [WebInvoke(
+                Method = "DELETE",
+                ResponseFormat = WebMessageFormat.Json,
+                UriTemplate = "catches?id={id}")
+        ]
+        Catch DeleteCatch(int id);
+
+        [WebInvoke(
+                Method = "PUT",
+                RequestFormat = WebMessageFormat.Json,
+                ResponseFormat = WebMessageFormat.Json,
+                UriTemplate = "catches")
+        ]
+        Catch UpdateCatch(Catch myCatch);
+
+
+
+    }
+
+
+    
+}
---
+++ ClassDemoCatches/Service1.svc.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.SessionState;
+using ModelLibrary;
+
+namespace ClassDemoCatches
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        private static List<Catch> catches = new List<Catch>()
+        {
+            new Catch(1,"Peter","Torsk",3.4,"Øresund",22),
+            new Catch(2,"Martin","Skalle",1.2,"Furesø",23),
+            new Catch(){Id=3,Navn = "Vibeke",Art="Ørred",Sted="Øresund",Uge=44,Vægt = 3.1}
+
+        };
+
+
+        public List<Catch> GetCatches()
+        {
+            return catches;
+        }
+
+        public Catch GetOneCatch(string id)
+        {
+            int idint = Int32.Parse(id);
+
+            return catches.Find(c => c.Id == idint);
+        }
+
+        public void AddCatch(Catch newCatch)
+        {
+            catches.Add(newCatch);
+        }
+
+        public Catch DeleteCatch(int id)
+        {
+            var deleteCatch = GetOneCatch(id.ToString());
+            if (deleteCatch != null)
+            {
+                catches.Remove(deleteCatch);
+                return deleteCatch;
+            }
+            
+            return null;
+        }
+
+        public Catch UpdateCatch(Catch myCatch)
+        {
+            var updateCatch = GetOneCatch(myCatch.Id.ToString());
+            if (updateCatch != null)
+            {
+                //updateCatch.Id = myCatch.Id;
+                updateCatch.Navn = myCatch.Navn;
+                updateCatch.Art = myCatch.Art;
+                updateCatch.Vægt = myCatch.Vægt;
+                updateCatch.Sted = myCatch.Sted;
+                updateCatch.Uge = myCatch.Uge;
+                return updateCatch;
+            }
+
+            return null;
+        }
+    }
+}
